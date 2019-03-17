82790885f42a73acf92b9f254e472e6dd8fc9a0b
kobg <48279370+kobg@users.noreply.github.com>
2019-03-17 18:36:19 +0100

    Update apex_stats_discordbot.py

--- apex_stats_discordbot.py
+++ apex_stats_discordbot.py
@@ -1,24 +1,17 @@
-#Mostly Thuleq's work
-
 import requests
-import json
 import discord
-import asyncio
 from discord.ext import commands
 import random
+import os
+import youtube_dl
 
-TOKEN = 'GET YOUR OWN BOT TOKEN'
-client = commands.Bot(command_prefix = '!')
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
+client = commands.Bot(command_prefix='!')
 
-def get_stats(user_name):
-    url = 'https://public-api.tracker.gg/apex/v1/standard/profile/5/{}'.format(user_name)
-    headers = {'GET YOUR OWN API KEY FROM apex.tracker.gg'}
-    r = requests.get(url, headers=headers)
 
-    response = r.json()
-    if 'errors' in response:
-        return None
-    legends = response['data']['children']
+def __parse__response(response):
+    response_json = response.json()
+    legends = response_json['data']['children']
     legend_dict = {}
     for legend in legends:
         legend_name = legend['metadata']['legend_name']
@@ -30,59 +23,77 @@ def get_stats(user_name):
             stat_dict[stat_name] = value
         legend_dict[legend_name] = stat_dict
     return legend_dict
+
+
+def get_stats(user_name):
+    url = 'https://public-api.tracker.gg/apex/v1/standard/profile/5/{}'.format(user_name)
+    headers = {'TRN-Api-Key': os.environ['TRN-Api-Key']}
+    r = requests.get(url, headers=headers)
+
+    try:
+        return __parse__response(r)
+    except:
+        return None
+
+
 @client.event
 async def on_ready():
-    print("rd")
+    print("Welcome to Apex Stats!")
+
+
 @client.command()
-async def ap_st(who):
-    embed=discord.Embed(title="Stats for {}".format(who),color=0xb90000)
+async def ap_st(who='<EMPTY_USERNAME>'):
+    embed = discord.Embed(title="Stats for **{}**:".format(who), color=0xb90000)
     stats = get_stats(who)
-    DamageValue = 0
-    KillsValue = 0
-    HeadshotsValue = 0
     if stats is None:
-        print("error msg")
-    for legend in stats:
-        legend_stats = stats[legend]
-        for stat, value in legend_stats.items():
-            if stat == "Damage":
-                DamageValue += value
-            if stat == "Kills":
-                KillsValue += value
-            if stat == "Headshots":
-                HeadshotsValue += value
-            print(legend,stat,value)
-    embed.add_field(name="Damage (on banners)", value=int(DamageValue), inline=False)
-    embed.add_field(name="Kills (on banners)", value=int(KillsValue), inline=False)
-    embed.add_field(name="Headshots (on banners)", value=int(HeadshotsValue), inline=False)
+        embed.description = "Sorry, couldn't find data for {}.".format(who)
+    else:
+        sum_dict = {
+            'Damage': 0,
+            'Kills': 0,
+            'Headshots': 0
+        }
+        for legend in stats:
+            legend_stats = stats[legend]
+            for key in legend_stats.keys():
+                if key in sum_dict:
+                    sum_dict[key] += int(legend_stats[key])
+        embed.add_field(name='Stats on banners',
+                        value='\n'.join(': '.join((key, str(value)))
+                                        for key, value in sum_dict.items()),
+                        inline=False)
     embed.set_footer(text="courtesy of apex.tracker.gg")
     await client.say(embed=embed)
+
 
 @client.command()
 async def ap_st_detailed(who):
-    embed=discord.Embed(title="Stats for {}".format(who),color=0xb90000)
+    embed = discord.Embed(title="Stats for **{}**".format(who), color=0xb90000)
     stats = get_stats(who)
-    helpvalue = 0
     if stats is None:
-        print("error msg")
+        embed.description = "Sorry, couldn't find data for {}.".format(who)
+
     for legend in stats:
         legend_stats = stats[legend]
-        embed.add_field(name=legend, value=legend_stats, inline=False)
-        #await client.say("{} {}".format(legend,legend_stats))
+        embed.add_field(name=legend,
+                        value='\n'.join(': '.join((key, str(int(value))))
+                                        for key, value in legend_stats.items()),
+                        inline=False)
     embed.set_footer(text="courtesy of apex.tracker.gg")
     await client.say(embed=embed)
 
+
+legend_list = ["Lifeline", "Caustic", "Bangalore", "Gibraltar", "Wraith", "Pathfinder", "Mirage", "Bloodhound"]
+
+
 @client.command()
 async def ap_teamcomp():
-    embed=discord.Embed(title="Your random teamcomp is: ",color=0xb90000)
-    tab = ["Lifeline","Caustic","Bangalore","Gibraltar","Wraith","Pathfinder","Mirage","Bloodhound"]
-    for x in range(0,3):
-        d = random.randint(0,len(tab)-1)
-        embed.add_field(name=tab[d], value="\u200b", inline=True)
-        del(tab[d])
+    embed = discord.Embed(title="Your random teamcomp is: ", color=0xb90000)
+    random.shuffle(legend_list)
+    embed.add_field(name='\n'.join(legend_list[:3]), value="\u200b", inline=True)
     embed.set_footer(text="Have fun!")
     await client.say(embed=embed)
-    tab = ["Lifeline","Caustic","Bangalore","Gibraltar","Wraith","Pathfinder","Mirage","Bloodhound"]
+
 
 @client.command(pass_context=True)
 async def clear(ctx, amount=100):
@@ -93,20 +104,73 @@ async def clear(ctx, amount=100):
     await client.delete_messages(messages)
     await client.say("Chat cleared!")
 
+
 @client.command()
 async def ap_help():
-    embed=discord.Embed(color=0x0e5634)
-    embed.add_field(name="!ap_st {ORIGIN_NAME}", value="Displays damage, kills, headshots (on_banner)", inline=False)
+    embed = discord.Embed(color=0x0e5634)
+    embed.add_field(name="!ap_st {ORIGIN_NAME}", value="Displays damage, kills, headshots (on_banner)",
+                    inline=False)
     embed.add_field(name="!ap_st_detailed {ORIGIN_NAME}", value="Displays all on-banner stats", inline=False)
     embed.add_field(name="!ap_teamcomp", value="Generates a random teamcomp", inline=False)
     embed.add_field(name="!clear", value="Clears 100 messages", inline=False)
     await client.say(embed=embed)
 
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+
+
+songs_url = {
+"tilt":"https://www.youtube.com/watch?v=GJDNkVDGM_s",
+"rocky":"https://www.youtube.com/watch?v=xSmYAdiXb5M",
+"benny":"https://www.youtube.com/watch?v=MK6TXMsvgQg",
+"tiger":"https://www.youtube.com/watch?v=btPJPFnesV4",
+"90s":"https://www.youtube.com/watch?v=BJ0xBCwkg3E",
+"dejavu":"https://youtu.be/dv13gl0a-FA?t=60",
+"freestyler":"https://www.youtube.com/watch?v=ymNFyxvIdaM",
+"sandstorm":"https://www.youtube.com/watch?v=y6120QOlsfU",
+"pole":"https://www.youtube.com/watch?v=UvRsDiOFdzA",
+"ziemia":"https://www.youtube.com/watch?v=fwkk6mJmjkg"
+}
+
 @client.command()
-async def music(what):
-    if what == "tilt":
-        await client.say("-play https://www.youtube.com/watch?v=GJDNkVDGM_s")
-    if what == "motivation":
-        await client.say("-play https://www.youtube.com/watch?v=xSmYAdiXb5M")
+async def songs():
+    embed = discord.Embed(title = "Current song list: ",color=0x0e5634)
+    embed.add_field(name="\n".join(songs_url),value="\u200b",inline = True)
+    embed.set_footer(text="Use !play <name> ")
+    await client.say(embed=embed)
+
+players = {}
+
+
+
+@client.command(pass_context=True)
+async def play(ctx,url):
+    server = ctx.message.server
+    connected = client.voice_client_in(server)
+    channel = ctx.message.author.voice.voice_channel
+    if not connected:
+        await client.join_voice_channel(channel)
+    voice_client = client.voice_client_in(server)
+    if url in songs_url:
+        player = await voice_client.create_ytdl_player(songs_url[url])
+    else:
+        player = await voice_client.create_ytdl_player(url)
+    players[server.id] = player
+    player.start()
+
+@client.command(pass_context=True)
+async def music(ctx,cmd):
+    id = ctx.message.server.id
+    if cmd == "pause":
+        players[id].pause()
+    elif cmd == "stop":
+        players[id].stop()
+    elif cmd == "resume":
+        players[id].resume()
+
 
 client.run(TOKEN)
